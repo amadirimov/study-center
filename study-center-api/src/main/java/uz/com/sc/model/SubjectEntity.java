98c37e27e093a2412fcaaa8a0978c1ba64aa9775
Anvar <madirimov83@gmail.com>
2019-02-06 16:05:59 +0500

    study center api structure is created

---
+++ study-center-api/src/main/java/uz/com/sc/model/SubjectEntity.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Anvar on 2/6/2019.
+ */
+public class SubjectEntity {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
